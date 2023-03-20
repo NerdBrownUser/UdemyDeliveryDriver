2ddf9e7bd5e27af79c3675a880414cf1a9fa0183
NerdBrownUser <kangjoon90@naver.com>
2023-03-20 17:17:30 +0900

    Unity Basic #2

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.0f;
+    [SerializeField] float moveSpeed = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Driver");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float steer = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(0.0f, move, 0.0f);
+        transform.Rotate(0.0f, 0.0f, -steer);
     }
 }
